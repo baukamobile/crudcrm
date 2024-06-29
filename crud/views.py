b74536499f990cb33c392d8d7f3da8f69d40865c
bauyrzhan <newnewernewest123@gmail.com>
2024-06-30 00:42:43 +0500

    table

--- crud/views.py
+++ crud/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 def home(request):
     tasks = Task.objects.all()
-    return render(request, 'home.html', context={'task':tasks})
+    return render(request, 'home.html', context={'tasks':tasks})
 
 def create_task(request):
     tasks = Task.objects.all()
